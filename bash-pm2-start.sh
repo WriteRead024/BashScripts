66f6cd151214997bb18cb59a25522667ced648b8
WriteRead024 <159483982+WriteRead024@users.noreply.github.com>
2024-02-09 14:57:45 -0500

    another tabbing space fix

--- bash-pm2-start.sh
+++ bash-pm2-start.sh
@@ -12,7 +12,7 @@ if [[ $SETOUTPUT != "" ]]
 then
     echo "Gnome Fedora 39 style Terminator terminal shell detected."
     echo "Setting tab title and proceeding to start pm2 management of app ${APPNAME}"
-	REMOTINATORRESULT=$(remotinator set_tab_title -t "$TABTITLETEXT")
+    REMOTINATORRESULT=$(remotinator set_tab_title -t "$TABTITLETEXT")
 else
     echo "'terminator' not found in shell set variables."
     echo "This is not a (Gnome Fedora 39) Terminator terminal command line."
