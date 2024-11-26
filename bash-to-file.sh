0cd17ee057465e1e04382f55b457ec695be1ce5d
WriteRead024 <rich@rwhifonline.net>
2024-11-26 16:59:35 -0500

    this was amusing

---
+++ bash-to-file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Rich W.
+# with GitHub Copilot
+# Mar. 8th 2024
+# Nov. 26th, 2024
+
+# -T-O-D-O- I license
+# -T-O-D-O- get working correctly somehow
+# 
+# 'script' is a pre-included command that does exactly what
+# this bash script was supposed to do.
+
+# Redirect all command line input and output to a file
+# timestamp=$(date +"%Y%m%d%H%M%S")
+# bash -c 'exec > >(tee -a output_$timestamp.bash_log) 2>&1'
+
+echo "Just use the 'script' command instead of this script."
+
