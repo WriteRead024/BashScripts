d7139b3ac1dd95173e52bb5318a40ef734331069
WriteRead024 <rich@rwhifonline.net>
2024-12-05 12:48:00 -0500

    new retitle-term-tab.sh

---
+++ retitle-term-tab.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Rich W.
+#Dec. 2024
+
+SETOUTPUT=$(set | grep "terminator")
+APPNAME="<>app<>"
+TABTITLETEXT="$1"
+REMOTINATORRESULT=""
+
+if [[ $SETOUTPUT != "" ]]
+then
+    echo "Terminator style terminal emulator shell detected."
+    echo "Setting tab title to ${TABTITLETEXT}"
+    REMOTINATORRESULT=$(remotinator set_tab_title -t "$TABTITLETEXT")
+else
+    echo "'terminator' not found in shell set variables."
+    echo "Not setting the terminal tab to '"$TABTITLETEXT"'."
+fi
